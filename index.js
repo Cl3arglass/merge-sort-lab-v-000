b8690b70808add9a9046fac506498526c133e2db
Holden Altaffer <haltaffer@gmail.com>
2019-10-10 22:55:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 function findMinAndRemove(firstHalf, secondHalf){
   let minfirstHalf = firstHalf[0]
   let minsecondHalf = secondHalf[0]
- 
+
   if(minfirstHalf < minsecondHalf){
     return firstHalf.shift()
   } else {
@@ -25,11 +25,11 @@ function mergeSort(array){
   let midpoint = array.length/2
   let firstHalf = array.slice(0, midpoint)
   let secondHalf = array.slice(midpoint, array.length)
- 
+
   if(array.length < 2){
     return array
   } else {
- 
+
     merge(mergeSort(firstHalf), mergeSort(secondHalf))
   }
 }
